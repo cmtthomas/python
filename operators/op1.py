e4191f4d95db792e7c49e1ba2f4651f88555a0f4
cmtthomas <42879368+cmtthomas@users.noreply.github.com>
2019-01-10 12:25:18 -0800

    Add files via upload

---
+++ operators/op1.py
@@ -0,0 +1,20 @@
+#op1.py cmt
+print(37 + 17)
+print(37 - 17)
+print(37 * 17)
+print(37 / 17)
+print(37 / 17)
+print(37 % 17)
+print(37 ** 17)
+print(37 // 17)
+print(37 // 17)
+
+"""
++ 	Addition 	x + y
+- 	Subtraction 	x - y
+* 	Multiplication 	x * y
+/ 	Division 	x / y
+% 	Modulus 	x % y
+** 	Exponentiation 	x ** y
+// 	Floor division 	x // y
+"""
---
+++ operators/ops1.py
@@ -0,0 +1,20 @@
+#ops1.py cmt
+print("37 + 17", 37 + 17)
+print("37 - 17", 37 - 17)
+print("37 * 17", 37 * 17)
+print("37.0 / 17.0", 37 / 17)
+print("37 / 17.", 37 / 17)
+print("37 % 17", 37 % 17)
+print("37 ** 17", 37 ** 17)
+print("37 // 17", 37 // 17)
+print("37.0 // 17.0", 37 // 17)
+
+"""
++ 	Addition 	x + y
+- 	Subtraction 	x - y
+* 	Multiplication 	x * y
+/ 	Division 	x / y
+% 	Modulus 	x % y
+** 	Exponentiation 	x ** y
+// 	Floor division 	x // y
+"""
