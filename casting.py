432a83ac5fa1eda5d85950efe707ddd9e3d3d101
cmtthomas <42879368+cmtthomas@users.noreply.github.com>
2019-01-10 12:27:12 -0800

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,11 @@
+#casting.py cmt
+
+a = int(-3.2)
+b =  float(-3)
+c =  str(3.149592)
+d =  str (bin(192)) #Decimal to Binary
+
+print(a,b,c,d)
+
+
+ 
---
+++ types1.py
@@ -0,0 +1,13 @@
+# types1.py
+x = -42000
+y = 2.89942
+z = 5+2j
+s = "I'm a String"
+#print values
+print(s, x, y, z)
+# print Types
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(z))
+
